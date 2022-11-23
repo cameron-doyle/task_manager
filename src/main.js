24a0aacf2ba954269942e9dcbcbb5331b749526b
Declan <dchino2@yahoo.com>
2022-11-24 09:54:38 +1030

    updated description

--- src/main.js
+++ src/main.js
@@ -27,17 +27,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		//? Description Validation (easy): Declan
 		/* Not Empty and longer than 15 characters */
-		const taskDescription = document.querySelector('#taskDescription');
+		const taskDescription = document.querySelector('#txt-new-task-description');
         let taskDescriptionValue = taskDescription.value;
 
-		if (taskDescriptionValue.length >= 15) {
+		if (taskDescriptionValue.length <= 15) {
 			taskDescription.classList.add("is-valid");
 			taskDescription.classList.remove("is-invalid");
-		  } else {
-			taskDescription.classList.add("is-invalid");
-			taskDescription.classList.remove("is-valid");
-			validationFail++;
-		  }
+			return 
+		  } 
 
 
 		//? Assign To Validation (easy): Tamika
