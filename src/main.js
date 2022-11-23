4ca1bdec6978639da3475efbec7cae15bab3402a
Declan <dchino2@yahoo.com>
2022-11-23 13:28:00 +1030

    description

--- src/main.js
+++ src/main.js
@@ -27,6 +27,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		//? Description Validation (easy): Declan
 		/* Not Empty and longer than 15 characters */
+		const taskDescription = document.querySelector('#taskDescription');
+        let taskDescriptionValue = taskDescription.value;
+
+		if (taskDescriptionValue.length >= 15) {
+			taskDescription.classList.add("is-valid");
+			taskDescription.classList.remove("is-invalid");
+		  } else {
+			taskDescription.classList.add("is-invalid");
+			taskDescription.classList.remove("is-valid");
+			validationFail++;
+		  }
 
 
 		//? Assign To Validation (easy): Tamika
