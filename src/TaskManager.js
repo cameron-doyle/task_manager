67af34a232313a49ae6d51acc1418e72d31a6327
Cameron Doyle <cameron.doyle465@gmail.com>
2022-11-24 13:35:25 +1100

    testing exmaple

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -36,7 +36,6 @@ class TaskManager{
 		const id = this.#taskList.length;
 		this.#taskList.push(taskObjFactory(id, name, description, assignedTo, dueDate, status));
 		//Call render()
-		//Don't mind me
 	}
 
 
