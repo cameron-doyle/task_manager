7e5b735a4104f72d4ea3f9e857be3098c6b37f47
Cameron Doyle <cameron.doyle465@gmail.com>
2022-11-24 14:46:15 +1100

    Added the task 7 functionality and did some other stuff I don't remember

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -12,7 +12,7 @@ class TaskManager{
 			AssignedTo: assignedTo,
 			DueDate: dueDate,
 			Status: status
-		};
+		}
 	}
 
 
@@ -33,18 +33,62 @@ class TaskManager{
 	/* task argument is a product of taskObjFactory */
 	/* TASK 7: call render() method */
 	addTask(name, description, assignedTo, dueDate, status){
-		const id = this.#taskList.length;
-		this.#taskList.push(taskObjFactory(id, name, description, assignedTo, dueDate, status));
-		//Call render()
+		let id = this.#taskList.length
+		//id = (id <= 0) ? 1:id;
+		//id ??= 1;
+		//Bad code, just do length + 1
+		id++;
+		this.#taskList.push(this.taskObjFactory(id, name, description, assignedTo, dueDate, status))
+		this.render()
 	}
 
 
-	/* TASK 7 */
+	/* TASK 7: done as team */
 	/* render() */
 	/* Render both calls the createTaskHTML and displays to DOM */
 	/* Loop through aray and call createTaskHTML for each object and pass object, the add child to card container */
 	/* ul ID: content-container */
+	render(){
+		//Get card container
+		const cardContainer = document.getElementById("content-container")
+		cardContainer.innerHTML = '' //Wipe existing cards
+
+		this.#taskList.forEach(task => {
+			//Prepare cardElement
+			let cardElement = document.createElement('li')
+			cardElement.className = "list-group-item"
+			cardElement.id = `task_${task.ID}`
+
+			//Create HTML card and append to ul
+			cardElement.innerHTML = this.createTaskHTML(task)
+			cardContainer.appendChild(cardElement)
+		})
+	}
 
 	/* createTaskHTML */
 	/* Takes an JSON object as an argument and spits out a card html with data */
+	createTaskHTML(taskObj){
+		//Html template literals
+		let date = taskObj.DueDate;
+		return `<div class="card" data-bs-toggle="modal" data-bs-target="#open-card">
+				<div class="card-header">
+					<h3>${taskObj.Name}</h3>
+					<p>${taskObj.AssignedTo}</p>
+				</div>
+				<div class="card-body">
+					<p>${taskObj.Description}</p>
+				</div>
+
+				<div class="card-footer">
+					<h5>${taskObj.Status}</h5>
+					<p>${ //Due Date
+						//Prepends a 0 if the day is less than 10.
+						(date.getDay() < 10) ? `0${date.getDay()}` : date.getDay()
+						}/${
+						//Prepends a 0 if month is less than 10
+						(date.getMonth() < 10) ? `0${date.getMonth()}` : date.getMonth()
+						}/${date.getFullYear()}</p>
+				</div>
+			</div>`
+	}
 }
